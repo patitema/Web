3d546c14bb357550e31534e63890b933e706120e
5&Tema <artemavhimovih@gmail.com>
2025-02-04 13:53:18 +0700

    Acc page

---
+++ Pages/Accaunt.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Личный кабинет</title>
+    <link rel="stylesheet" href="/Styles/Accaunt.css">
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <img class="logo" src="/Images/Logo.png" alt="">
+            <ul class="PageList">
+                <li><a href="/index.php">Главная</a></li>
+                <li><a href="/Pages/Catalog.php">Каталог</a></li>
+            </ul>
+            <button class="login"><img src="" alt=""></button>
+        </div>
+    </nav>
+    <main>
+        <div class="Container">
+            <div class="info">
+            <div class="info_head"><h2>О компании</h2></div>
+            <div class="info_main"><p>Simply-Learn - сибирская компания с 2-х летним стажем работы в сфере продажи курсов. 
+                На нашем счету 200+ тыс клиентов, 1000+ препадователей и более 2-х тысяч проданых курсов. <br><br>
+            Основатель Simply-Learn: Авхимович А.П - также непосредственно учавствующий в разработке и функционировании проекта.</p>
+            </div>
+            <div class="info_foot"><p>Оффис компании находится в г.Барнаул по улице "Юрина, 194а 2э"</p></div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <span></span>
+            <ul class="foot-menu">
+                <li><a href="">Задать вопрос</a></li>
+                <li><a href="">Пользовательское соглашение</a></li>
+                <li><a href="">Создать курс</a></li>
+                <li><a href="">Контакты</a></li>
+                <li><a href="">О нас</a></li>
+            </ul>
+            <span></span>
+            <p>© Simply-learn 2022–2024. Все права защищены.</p>
+        </div>
+    </footer>
+</body>
+</html>
